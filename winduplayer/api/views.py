f77472a6e3ef4a65ebba91e553068c3a616b4da7
RomainDLS <romain.dls@live.fr>
2017-04-17 22:34:52 +0200

    Add omxplayer play command

--- winduplayer/api/views.py
+++ winduplayer/api/views.py
@@ -2,12 +2,23 @@
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
+from core.models import Movie
+import os
 
 
 @api_view(['POST'])
 def global_cmd(request, movie_pk):
     response = {}
-    print('global_cmd_received', movie_pk)
-    response['movie_id'] = movie_pk
-    response['command'] = request.data['command']
+    command = request.data['command']
+    try:
+        movie = Movie.objects.get(pk=movie_pk)
+        path = movie.path
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    if command == 'play':
+        response['path'] = path
+        os.system("omxplayer -r {}".format(path))
+
     return Response(response)
